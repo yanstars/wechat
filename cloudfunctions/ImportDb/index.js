e93c1778be1b2eb79f40d44316d2de01b2bb1fe7
yanxb <yanxb8257@fxiaoke.com>
2019-09-07 17:14:36 +0800

    import db

--- cloudfunctions/ImportDb/index.js
+++ cloudfunctions/ImportDb/index.js
@@ -1,16 +1,35 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+var rp = require('request-promise');
 
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  console.log("云端收到的凭证：", event.token)
+  var token = event.token
+  var filename = event.filename
+  var type = event.type
+  var options = {
+    method: 'POST',
+    uri: 'https://api.weixin.qq.com/tcb/databasemigrateimport?access_token=' + token,
+    // uri: `https://api.weixin.qq.com/tcb/databasemigrateimport?access_token=token`,
+    body: {
+      "env": "yanstars-qsjfo",
+      "collection_name": "users",
+      "file_path": filename,
+      "file_type": type,
+      "stop_on_error": false,
+      "conflict_mode": 1
+    },
+    json: true
+  };
+  return rp(options)
+    .then(function (res) {
+      return res
+    })
+    .catch(function (err) {
+      return err
+    });
 }
